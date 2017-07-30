14022bfcc07aca476ff7f93712c40631e7c4953a
Anicky <jeremie.jalouzet@gmail.com>
2017-07-30 20:06:25 +0200

    [Camera] Block camera when player approches to screen edges (Fix #2)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,29 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
 
     public Transform target;
     public float speed = 0.1f;
-    Camera myCamera;
+    Camera mainCamera;
+    float mapWidth = 480; // @TODO : Fix by get real map width
+    float mapHeight = 320; // @TODO : Fix by get real map width
 
-	// Use this for initialization
-	void Start () {
-        myCamera = GetComponent<Camera>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        myCamera.orthographicSize = Screen.height / 4f;
-        if(target) {
-            float distance = Vector3.Distance(target.position, new Vector3(90,-90,-10));
-            if (distance < 10)
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.orthographicSize = Screen.height / 2f;
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+        float cameraBorderLeft = (cameraWidth / 2) - 12;
+        float cameraBorderRight = mapWidth - (cameraWidth / 2) - 16;
+        float cameraBorderUp = -(cameraHeight / 2);
+        float cameraBorderDown = -mapHeight + (cameraHeight / 2) + 2;
+        if (target)
+        {
+            float toX = target.position.x;
+            float toY = target.position.y;
+            if (target.position.x < cameraBorderLeft)
             {
-                transform.position = Vector3.Lerp(transform.position, target.position, speed) + new Vector3(0, 0, -10);
-            } else
-            {
-                transform.position = new Vector3(0, 0, -10);
+                toX = cameraBorderLeft;
             }
+            else if (target.position.x > cameraBorderRight)
+            {
+                toX = cameraBorderRight;
+            }
+            if (target.position.y > cameraBorderUp)
+            {
+                toY = cameraBorderUp;
+            }
+            else if (target.position.y < cameraBorderDown)
+            {
+                toY = cameraBorderDown;
+            }
+            transform.position = Vector3.Lerp(
+                new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                new Vector3(toX, toY, transform.position.z)
+                , speed
+                );
         }
-	}
+    }
 }
