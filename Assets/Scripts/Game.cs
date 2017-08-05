28d5fc0135911aec4a5dc4e182431bee3d91e70c
Anicky <jeremie.jalouzet@gmail.com>
2017-08-05 13:58:27 +0200

    [Menu] Prototype

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	List<Hero> heroes;
+
+	// Use this for initialization
+	void Start () {
+
+		heroes = new List<Hero>();
+
+		Hero heroCyril = new Hero ("Cyril");
+		heroCyril.hp = 540;
+		heroCyril.hpMax = 720;
+
+		heroes.Add(heroCyril);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,13 @@
+﻿public class Hero {
+
+	public string name;
+	public int hp;
+	public int hpMax;
+	public int sp;
+	public int spMax;
+
+	public Hero(string name) {
+		this.name = name;
+	}
+
+}
