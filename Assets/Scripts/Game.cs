4f1a7a8a3ebcd5fbeb32b2c941791a5026fd7474
Anicky <jeremie.jalouzet@gmail.com>
2017-08-07 20:39:20 +0200

    [Menu] Close transition on menu not working (fix #12)

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
 
 	List<Hero> heroes;
-	public Canvas menu;
+	public Menu menu;
 	private KeyCode keyCodeToOpenMenu = KeyCode.Escape;
-	public bool isMenuOpened = false;
 
 	// Use this for initialization
 	void Start () {
 
-		heroes = new List<Hero>();
+        heroes = new List<Hero>();
 
 		Hero heroCyril = new Hero ("Cyril");
 		heroCyril.hp = 540;
@@ -27,24 +25,12 @@ public class Game : MonoBehaviour {
 	}
 
 	void checkMenu() {
-
 		if (Input.GetKeyDown (keyCodeToOpenMenu)) {
-			isMenuOpened = !isMenuOpened;
-
-			if (isMenuOpened) {
-				menu.enabled = true;
-                Animator anim = menu.GetComponent<Animator>();
-                anim.SetBool("is_opened", true);
-                var thePlayer = FindObjectOfType<PlayerController>();
-				thePlayer.disableMovement();
+			if (menu.isOpened) {
+				menu.close ();
 			} else {
-				menu.enabled = false;
-                Animator anim = menu.GetComponent<Animator>();
-                anim.SetBool("is_opened", false);
-                var thePlayer = FindObjectOfType<PlayerController>();
-				thePlayer.enableMovement();
+				menu.open ();
 			}
 		}
-
 	}
 }
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    public bool isOpened = false;
+    private Canvas canvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        canvas = this.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void open()
+    {
+        StartCoroutine(openMenu());
+    }
+
+    public void close()
+    {
+        StartCoroutine(closeMenu());
+    }
+
+    IEnumerator openMenu()
+    {
+        var thePlayer = FindObjectOfType<PlayerController>();
+        thePlayer.disableMovement();
+        canvas.enabled = true;
+        Animation anim = GetComponent<Animation>();
+        anim.Play("Menu_Open");
+        do
+        {
+            yield return null;
+        } while (anim.isPlaying);
+        isOpened = true;
+    }
+
+    IEnumerator closeMenu()
+    {
+        Animation anim = GetComponent<Animation>();
+        anim.Play("Menu_Close");
+        do
+        {
+            yield return null;
+        } while (anim.isPlaying);
+        isOpened = false;
+        canvas.enabled = false;
+        var thePlayer = FindObjectOfType<PlayerController>();
+        thePlayer.enableMovement();
+    }
+
+}
