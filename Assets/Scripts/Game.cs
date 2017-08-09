bc56f189966180aff07989646b3acbde4e9be4b1
Anicky <jeremie.jalouzet@gmail.com>
2017-08-09 20:01:51 +0200

    [Config] Adjust game starting stats for heroes

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,27 +1,21 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
 
-	public Hero[] heroes;
-	public Menu menu;
-	private KeyCode keyCodeToOpenMenu = KeyCode.Escape;
+    public Hero[] heroes;
+    public Menu menu;
+    private KeyCode keyCodeToOpenMenu = KeyCode.Escape;
     private static bool gameExists = false;
     public Player player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         menu.game = this;
-        heroes = new Hero[] { null, null, null, null};
-
-		Hero heroCyril = new Hero ("Cyril");
-        heroCyril.lv = 1;
-		heroCyril.hp = 540;
-		heroCyril.hpMax = 720;
-        heroCyril.ap = 120;
-        heroCyril.apMax = 140;
-
-        heroes[0] = heroCyril;
+        heroes = new Hero[] { null, null, null, null };
+        initGame();
         if (!gameExists)
         {
             gameExists = true;
@@ -32,19 +26,45 @@ public class Game : MonoBehaviour {
             Destroy(gameObject);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		checkMenu ();
-	}
 
-	void checkMenu() {
-		if (Input.GetKeyDown (keyCodeToOpenMenu)) {
-			if (menu.isOpened) {
-				menu.close ();
-			} else {
-				menu.open ();
-			}
-		}
-	}
+    void initGame()
+    {
+        Hero heroCyril = new Hero("Cyril");
+        heroCyril.lv = 1;
+        heroCyril.hp = 750;
+        heroCyril.hpMax = 750;
+        heroCyril.ap = 80;
+        heroCyril.apMax = 80;
+
+        Hero heroMax = new Hero("Max");
+        heroMax.lv = 2;
+        heroMax.hp = 830;
+        heroMax.hpMax = 830;
+        heroMax.ap = 60;
+        heroMax.apMax = 60;
+
+        heroes[0] = heroCyril;
+        heroes[1] = heroMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkMenu();
+    }
+
+    void checkMenu()
+    {
+        if (Input.GetKeyDown(keyCodeToOpenMenu))
+        {
+            if (menu.isOpened)
+            {
+                menu.close();
+            }
+            else
+            {
+                menu.open();
+            }
+        }
+    }
 }
