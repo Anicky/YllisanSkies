bfc96f694470d96afa00a6d7e81470c40ddb028f
Anicky <jeremie.jalouzet@gmail.com>
2017-08-19 15:36:22 +0200

    [Mapping] Improve mapping for test maps

--- Assets/TileSet/Maison1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="chipset12" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../ChipSet/chipset12.png" trans="007575" width="520" height="512"/>
-</tileset>
--- Assets/TileSet/Maison2.tsx
+++ Assets/TileSet/Maison2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="town01_b2" tilewidth="16" tileheight="16" tilecount="480" columns="30">
- <image source="../ChipSet/town01_b2.png" trans="ff8cb4" width="480" height="256"/>
+<tileset name="town01_b2" tilewidth="16" tileheight="16" tilecount="960" columns="30">
+ <image source="../ChipSet/town01_b2.png" trans="ff8cb4" width="480" height="320"/>
 </tileset>
