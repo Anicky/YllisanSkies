1e3907c55dfe51a00a4f415d6125e0526fa7fcc9
Anicky <jeremie.jalouzet@gmail.com>
2017-08-28 16:42:46 +0200

    [Mapping] Animate door opening

---
+++ Assets/Scripts/LoadMapOpeningDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMapOpeningDoor : LoadMap
+{
+
+    public AudioClip doorSound;
+
+    protected override void doActionWhenTriggered()
+    {
+        Animator anim = GetComponent<Animator>();
+        if (anim)
+        {
+            anim.SetTrigger("Open");
+        }
+        // @TODO : Move player 
+        // @TODO : Load map when animation is finished
+    }
+}
