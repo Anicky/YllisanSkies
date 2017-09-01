8739a549ebf28889f34589d72a564e0c47240f92
Anicky <jeremie.jalouzet@gmail.com>
2017-09-01 19:53:32 +0200

    [Utils] Create MouseUtils class

---
+++ Assets/Scripts/Utils/MouseUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MouseUtils
+{
+    public const int INPUT_CLICK_LEFT = 0;
+    public const int INPUT_CLICK_RIGHT = 1;
+    public const int INPUT_CLICK_MIDDLE = 2;
+
+    public static Vector2 getPositionFromMouse()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
