eed731401e484c33a5a07ac25fa7af700eb2260e
Anicky <jeremie.jalouzet@gmail.com>
2017-09-02 12:13:56 +0200

    [Utils] Add function in GameObjectUtils to search an object by name in a list

---
+++ Assets/Scripts/Utils/GameObjectUtils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class GameObjectUtils
+{
+
+    public static GameObject searchByNameInList(GameObject[] list, string name)
+    {
+        return list.Where(obj => obj.name == name).SingleOrDefault();
+    }
+}
