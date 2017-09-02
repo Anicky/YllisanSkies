69b6c2d0aaa749095c6ed70db104fd2a6cfd64a7
Anicky <jeremie.jalouzet@gmail.com>
2017-09-02 10:57:54 +0200

    [Mapping] Add battle background 01 for Forest of Hopes

---
+++ Tiled/TileSet/Forest_001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Forest_001" tilewidth="16" tileheight="16" tilecount="480" columns="30">
+ <image source="Forest_001.png" width="480" height="256"/>
+</tileset>
