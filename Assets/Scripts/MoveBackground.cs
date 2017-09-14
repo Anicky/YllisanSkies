fa5e11d704224b2ef379e0cbd9bd6b237958b66e
Anicky <jeremie.jalouzet@gmail.com>
2017-09-14 19:44:02 +0200

    [Mapping] Moving clouds : Fix background ratio when changing scene

--- Assets/Scripts/MoveBackground.cs
+++ Assets/Scripts/MoveBackground.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using RaverSoft.YllisanSkies.Utils;
 
 namespace RaverSoft.YllisanSkies
 {
@@ -24,16 +26,31 @@ namespace RaverSoft.YllisanSkies
         void Start()
         {
             game = GameObject.Find("Game").GetComponent<Game>();
+            SceneManager.activeSceneChanged += OnSceneChange;
             backgroundRenderer = GetComponent<Renderer>();
-            map = GameObject.Find("Map").GetComponent<Tiled2Unity.TiledMap>();
-            float ratio = map.GetMapWidthInPixelsScaled() / backgroundRenderer.material.mainTexture.width;
-            backgroundRenderer.material.mainTextureScale = new Vector2(ratio, ratio);
+            initialize(SceneManager.GetActiveScene().GetRootGameObjects());
+        }
+
+        private void initialize(GameObject[] gameObjects)
+        {
+            GameObject mapObject = GameObjectUtils.searchByNameInList(gameObjects, "Map");
+            if (mapObject && backgroundRenderer)
+            {
+                map = mapObject.GetComponent<Tiled2Unity.TiledMap>();
+                float ratio = map.GetMapWidthInPixelsScaled() / backgroundRenderer.material.mainTexture.width;
+                backgroundRenderer.material.mainTextureScale = new Vector2(ratio, ratio);
+            }
+        }
+
+        private void OnSceneChange(Scene previousScene, Scene currentScene)
+        {
+            initialize(currentScene.GetRootGameObjects());
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (!game.stopEvents)
+            if (!game.stopEvents && map)
             {
                 backgroundRenderer.material.mainTextureOffset = new Vector2(frameCounter * scrollSpeedX * Time.deltaTime, frameCounter * scrollSpeedY * Time.deltaTime);
                 frameCounter++;
